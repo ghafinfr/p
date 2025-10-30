e4726b2f80324fee89e203708cad4c0f9aa7762a
“ghafinfr” <ghafinfr@gmail.com>
2025-10-30 09:41:13 +0700

    modif percobaan 4

--- nilaiKelompok12.java
+++ nilaiKelompok12.java
@@ -3,26 +3,31 @@ import java.util.Scanner;
 public class nilaiKelompok12 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int i = 1; 
+        int nKelompok = sc.nextInt(); ;
+        float totalNilai, rataNilai;
+        float rataTertinggi = 0; 
+        int kelompokTertinggi = 0;
 
-        int i = 1;             
-        int nKelompok = 6;     
-        float totalNilai, rataNilai;  
         while (i <= nKelompok) {
             System.out.println("Kelompok " + i);
-
             totalNilai = 0; 
-            for (int j = 1; j <= 5; j++) { 
+            for (int j = 1; j <= 5; j++) {
                 System.out.print("Nilai dari Kelompok Penilai " + j + ": ");
                 float nilai = sc.nextFloat();
-                totalNilai += nilai; 
+                totalNilai += nilai;
             }
-
             rataNilai = totalNilai / 5;
             System.out.println("Kelompok " + i + ": nilai rata-rata = " + rataNilai);
             System.out.println();
-
-            i++; 
+            if (rataNilai > rataTertinggi) {
+                rataTertinggi = rataNilai;
+                kelompokTertinggi = i;
+            }
+            i++;
         }
+        System.out.println("Kelompok dengan rata-rata tertinggi adalah Kelompok " 
+                            + kelompokTertinggi + " dengan nilai rata-rata " + rataTertinggi);
 
         sc.close();
     }
